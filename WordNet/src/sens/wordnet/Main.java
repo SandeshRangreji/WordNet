9db68c0514b303f4d0eb60c5ed35d5b4ccd2f1b2
Sandesh Rangreji <sandesh.rangreji@gmail.com>
2021-06-28 20:27:13 +0530

    solve errors in readSynset, write readHypernyms for WordNet API

--- WordNet/src/sens/wordnet/Main.java
+++ WordNet/src/sens/wordnet/Main.java
@@ -1,9 +1,5 @@
 package sens.wordnet;
 
-import sens.wordnet.services.Bag;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
--- WordNet/src/sens/wordnet/main/WordNet.java
+++ WordNet/src/sens/wordnet/main/WordNet.java
@@ -1,10 +1,15 @@
 package sens.wordnet.main;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+
+import sens.wordnet.services.DiGraph;
+import sens.wordnet.services.DirectedCycle;
 
 public class WordNet {
 
@@ -28,10 +33,11 @@ public class WordNet {
             throw new IllegalArgumentException("Input is null.");
         return noun2ID.containsKey(word);           //returns if the word is present in noun2ID 
     }
-    public String readLine() {
+
+    public String readLine(Scanner in) {
         String line;
         try {
-            line = scanner.nextLine();
+            line = in.nextLine();
         }
         catch (NoSuchElementException e) {
             line = null;
@@ -39,33 +45,72 @@ public class WordNet {
         return line;
     }
 
-    public int readSynsets(String synsets) {
-        if (synsets == null)
-            throw new IllegalArgumentException("Argument is null");
-        File file = new File("synsets.txt");
-        Scanner in= new Scanner(file);
-        int count = 0;
-        while (in.hasNextLine()) {
-            count++;
-            String combined = in.readLine();
-            String separated[]= combined.split(",");        //segregates id and sentence in synset.txt
-            int id = Integer.parseInt(separated[0]);
-            ID2Noun.put(id, separated[1]);                  //pushes into hashmap id and sentence
-            String words[] = separated[1].split(" ");       //segregates into words
-            for (String n : words) {
-                if (noun2ID.get(n) != null) {
-                    Bag<Integer> bag = noun2ID.get(n);
-                    bag.add(id);
-                } else {
-                    Bag<Integer> bag = new Bag<Integer>();
-                    bag.add(id);
-                    noun2ID.put(n, bag);
+    private void readSynsets() {
+        File file = new File("D:/WordNet/src/sens/wordnet/assets/synsets.txt");
+        Scanner in;
+        try {
+            in = new Scanner(file);
+            while (in.hasNextLine()) {
+                String combined = readLine(in);
+                String[] separated= combined.split(",");        //segregates id and sentence in synset.txt
+                if (separated.length < 2) {
+                    continue;
+                }
+                ++noVertices;
+                int id = Integer.parseInt(separated[0]);
+                ID2Noun.put(id, separated[1]);                  //pushes into hashmap id and sentence
+                String words[] = separated[1].split(" ");       //segregates into words
+                for (String word : words) {
+                    ArrayList<Integer> ids = noun2ID.get(word);
+                    if (ids != null) {
+                        ids.add(id);
+                    } else {
+                        ArrayList<Integer> nids = new ArrayList<Integer>();
+                        nids.add(id);
+                        noun2ID.put(word, nids);
+                    }
                 }
             }
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        in.close();
-        return count;
     }
 
+    private void readHypernyms() {
+        File file = new File("D:/WordNet/src/sens/wordnet/assets/hypernyms.txt");
+        Scanner in;
+        String line;
+        DiGraph digraph = new DiGraph(this.noVertices);
+        try {
+            in = new Scanner(file);
+            while (in.hasNextLine()) {
+                line = readLine(in);
+                String[] seperatedStrings = line.split(",");
+                if (seperatedStrings.length < 2) {
+                    continue;
+                }
+                int start = Integer.parseInt(seperatedStrings[0]);
+                for (int i = 1; i < seperatedStrings.length; ++i) {
+                    digraph.addEdge(start, Integer.parseInt(seperatedStrings[i]));
+                }
+            }
+            DirectedCycle dc = new DirectedCycle(digraph);
+            if (dc.hasCycle()) {
+                throw new IllegalArgumentException("Cycle detected");
+            }
+            int noRoot = 0;
+            for (int i = 0; i < digraph.noVertices(); ++i) {
+                if (digraph.outDegree(i) == 0) {
+                    ++noRoot;
+                    if (noRoot > 1) {
+                        throw new IllegalArgumentException("More than 1 root");
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
