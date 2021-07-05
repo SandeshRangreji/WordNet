ac50d265ecc53249d01fd51f85263fdc179db25d
sami7622 <sameerviswabharathi@gmail.com>
2021-07-05 11:57:50 +0530

    added BFS and INIT methods

--- WordNet/src/sens/wordnet/utils/SAP.java
+++ WordNet/src/sens/wordnet/utils/SAP.java
@@ -41,6 +41,61 @@ public class SAP {
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
         //shortest common ancestor
     }
+    private void bfs(Queue<Integer> q1, Queue<Integer> q2){
+        while(!q1.isEmpty() || !q2.isEmpty()){
+            if(!q1.isEmpty()){
+                int v=q1.dequeue();
+                int s=distTo1[v]+distTo2[v];
+                if(marked2[v]){
+                    if( s < length || length==-1){
+                        ancestor=v;
+                        length=s;
+                    }
+                }
+                if(distTo1[v] < length || length ==-1){
+                    for(int a : copyG.adj(v)){
+                        if(!marked1[a]){
+                            distTo1[a]=distTo1[v]+1;
+                            marked1[a]=true;
+                            stack1.push(a);
+                            q1.enqueue(a);
+                        }
+                    }
+                }
+            }
+            if(!q2.isEmpty()){
+                int v = q2.dequeue();
+                int s = distTo1[v]+distTo2[v];
+                if(marked1[v]){
+                    if( s< length || length==-1){
+                        ancestor=v;
+                        length=s;
+                    }
+                }
+                if (distTo2[v] < length || length==-1){
+                    for(int a: copyG.adj(v)){
+                        if(!marked2[a]){
+                            distTo2[a]=distTo2[v]+1;
+                            marked2[a] = true;
+                            stack2.push(a);
+                            q2.enqueue(a);
+                        }
+                    }
+                }
+            }
+        }
+        init();
+    }
+    private void init(){
+        while(!stack1.isEmpty()){
+            int x=stack1.pop();
+            marked1[x]=false;
+        }
+        while(!stack2.isEmpty()){
+            int x=stack2.pop();
+            marked2[x]=false;
+        }
+    }
 
     public static void main(String[] args){
         //driver for unit testing
